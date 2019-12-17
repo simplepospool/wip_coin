d4875697867443e78875faa22c16ffd24b460024
Function-Coin <58431513+Function-Coin@users.noreply.github.com>
2019-12-17 23:49:28 +0000

    Create uninstall_wipc.sh

---
+++ scripts/uninstall_wipc.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+COIN='wipcoin'
+TICKER='wipc'
+
+
+set -e
+
+if [ $# -lt 1 ]; then
+  echo 1>&2 "$0: Supply the node id to uninstall!"
+  exit 2
+fi
+
+id=$(printf '%03d' "${1}")
+
+systemctl disable "$TICKER-${id}"
+systemctl stop "$TICKER-${id}"
+rm -r "/mnt/$TICKER/${id}"
+rm "/etc/systemd/system/$TICKER-${id}.service"
